3766a299e1385437b237dd80523dd88ec37ab541
Mehedihasan023 <mh20973@gmail.com>
2021-08-31 22:14:56 +0600

    function updated

--- js/calculation.js
+++ js/calculation.js
@@ -95,80 +95,44 @@ document.getElementById('btNo=').addEventListener
         }
         math.innerText=total;
     }
-
     else if (mathvalue.indexOf('-') !=  -1){
         const sub = mathvalue.split('-');
-        function substraction(a,b){
-            let c= a-b;
-            return c;
-        }
-        let total1= [0];
-        let temp = [0];
-        let res =[0];
-        for(const element1 of sub){
-            let elementValue1 = parseFloat(element1);
-            // total1 =  (elementValue)-(total1) ;
-            temp[0] = elementValue1;
-            temp[1] = res;
-            let a  = temp[0];
-            let b  = parseFloat(temp[1]);
-            res=  (a) - (b);
-            // if( temp[0]=temp[1]){
-            if(element1 < sub.length){
-            // total1 = substraction(b,a);
-            res = substraction(b,a);
-            math.innerText = res;
-            // total1 = substraction(b,a);
+        function division(a,b) {
+            let res =0;
+            for(let i=1;i< a.length;i++){
+                const elementvalue = a[i];
+                
+                let element = parseFloat(elementvalue);
+                
+                    res = b- element;
+                    b=res;
             }
-            else{
-                total1 = substraction(b,a);
-                math.innerText = total1;
-            }
-                // math.innerText = total1;
-            // }
-            
-            // res =parseFloat(total1);
-            // res = substraction(total1,temp);
-            // total1= res;
+            return res;
         }
-        // math.innerText = res;
+        var fstnumtext = sub[0];
+        let fstnum= parseFloat(fstnumtext);
+        let temp = fstnum;
+        let finalres = division(sub,temp);
+        math.innerText=finalres;
     }
-
     else if (mathvalue.indexOf('/') !=  -1){
-        const division = mathvalue.split('/');
-        function a (b,c){
-            let total2 = b/c;
-            math.innerText=total2;
-        }
-        let total= 1;
-        for(const element of division){
-            let elementValue = parseFloat(element);
-            if(elementValue != total){
-            a(total,elementValue);
+        const div = mathvalue.split('/');
+        function division(a,b) {
+            let res =0;
+            for(let i=1;i< a.length;i++){
+                const elementvalue = a[i];
+                
+                let element = parseFloat(elementvalue);
+                
+                    res = b / element;
+                    b=res;
             }
-            else if (elementValue == total){
-                total = elementValue / total;
-                math.innerText=total;
-            }  
-            total = elementValue / total;
+            return res;
         }
+        let fstnumtext = div[0];
+        let fstnum= parseFloat(fstnumtext);
+        let temp = fstnum;
+        let finalres = division(div,temp);
+        math.innerText=finalres;
     }
-    // else if (mathvalue.indexOf('-') !=  -1){
-    //     const plus = mathvalue.split('-');
-    //     var a = plus[0];
-    //     var b = plus[1];
-    //     var num1 = parseFloat(a);
-    //     var num2 = parseFloat(b);
-    //     var total = num1 - num2;
-    //     math.innerText=total;
-    // }
-    // else if (mathvalue.indexOf('/') !=  -1){
-    //     const plus = mathvalue.split('/');
-    //     var a = plus[0];
-    //     var b = plus[1];
-    //     var num1 = parseFloat(a);
-    //     var num2 = parseFloat(b);
-    //     var total = num1 / num2;
-    //     math.innerText=total;
-    // }
 })
